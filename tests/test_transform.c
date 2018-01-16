ef643ca7c940b8ed60a2132bd548d32d86a45752
Kaiwen Xu <kevin@kevxu.net>
2018-01-15 21:19:57 -0800

    Test for transform speed.

--- tests/test_transform.c
+++ tests/test_transform.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "transform.h"
 #include "udpmask.h"
@@ -14,13 +15,26 @@ void print_mask(struct um_transform *ctx) {
 
 int main(void)
 {
+    printf("MASK_LEN: %d\n", MASK_LEN);
+
     struct um_transform tran;
     memset(&tran, 0, sizeof(tran));
 
-    printf("MASK_LEN: %d\n", MASK_LEN);
+    int iter = 10000;
+    struct timeval t_start, t_end;
+    double t_diff;
+
+    gettimeofday(&t_start, NULL);
+    for (int i = 0; i < iter; i++) {
+        check_gen_mask(&tran);
+        tran.mask_updated = 0;
+    }
+    gettimeofday(&t_end, NULL);
+    t_diff = (double) ((t_end.tv_sec - t_start.tv_sec) * 1e6 + (t_end.tv_usec - t_start.tv_usec));
+    printf("Time for check_gen_mask, %d iterations: %f us\n", iter, t_diff);
+    printf("Time for check_gen_mask, 1 iterations: %f us\n", t_diff / iter);
 
     check_gen_mask(&tran);
-
     print_mask(&tran);
 
     unsigned char buf[1024] = "ABCDABCDABCDABCDCD";
@@ -32,6 +46,15 @@ int main(void)
     }
     printf("\n");
 
+    gettimeofday(&t_start, NULL);
+    for (int i = 0; i < iter; i++) {
+        maskbuf(&tran, buf, buflen);
+    }
+    gettimeofday(&t_end, NULL);
+    t_diff = (double) ((t_end.tv_sec - t_start.tv_sec) * 1e6 + (t_end.tv_usec - t_start.tv_usec));
+    printf("Time for maskbuf, %d iterations: %f us\n", iter, t_diff);
+    printf("Time for maskbuf, 1 iterations: %f us\n", t_diff / iter);
+
     buflen = maskbuf(&tran, buf, buflen);
 
     printf("maskbuf:");
@@ -39,6 +62,15 @@ int main(void)
         printf(" 0x%02X", buf[i]);
     }
     printf("\n");
+
+    gettimeofday(&t_start, NULL);
+    for (int i = 0; i < iter; i++) {
+        unmaskbuf(&tran, buf, buflen);
+    }
+    gettimeofday(&t_end, NULL);
+    t_diff = (double) ((t_end.tv_sec - t_start.tv_sec) * 1e6 + (t_end.tv_usec - t_start.tv_usec));
+    printf("Time for unmaskbuf, %d iterations: %f us\n", iter, t_diff);
+    printf("Time for unmaskbuf, 1 iterations: %f us\n", t_diff / iter);
 
     buflen = unmaskbuf(&tran, buf, buflen);
 
